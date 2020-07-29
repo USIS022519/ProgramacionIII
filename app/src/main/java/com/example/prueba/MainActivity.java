e3f93ab1e3e96ea86e3b9e7a4f4679695d7c49b2
Luis Enrique Hernandez <sarco_luis@hotmail.com>
2020-07-29 13:03:06 -0600

    Creacion de una calculadora basica de suma -> Controles basicos

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.prueba;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void calcular(View view){
+        TextView tempVal = (TextView)findViewById(R.id.txtnum1);
+        double num1 = Double.parseDouble(tempVal.getText().toString());
+
+        tempVal = (TextView)findViewById(R.id.txtnum2);
+        double num2 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = num1 + num2;
+
+        tempVal = (TextView)findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
     }
 }
