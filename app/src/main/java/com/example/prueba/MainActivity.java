2a1751f54b306818ffa6e3dfd1bea7f5afcf3758
Luis Enrique Hernandez <sarco_luis@hotmail.com>
2020-08-12 10:04:50 -0600

    Agregue el envento onclick directamente desde Java y se borro la llamada desde el xml

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.prueba;
 
 import android.app.Activity;
+import android.net.wifi.aware.PublishConfig;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    public Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCalcular = (Button)findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                procesar(view);
+            }
+        });
     }
     public void procesar(View vista){
         try {
