e8aeca89645cfb0090844ecd2cfaec19e0e656d8
Luis Enrique Hernandez <sarco_luis@hotmail.com>
2020-08-21 13:44:40 -0600

    Trabajando con acceso al dispositivo - sensor de proximidad...

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.prueba;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -8,58 +13,60 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    //direcciones direccion = new direcciones();
-    direcciones_pais miDireccion = new direcciones_pais();
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
 
-       @Override
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    @Override
+       protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost tbhConversores = (TabHost)findViewById(R.id.tbhDirecciones);
-        tbhConversores.setup();
-
-        tbhConversores.addTab(tbhConversores.newTabSpec("Direccion").setContent(R.id.tabDireccion).setIndicator("DIRECCION", null));
-        tbhConversores.addTab(tbhConversores.newTabSpec("Pais").setContent(R.id.tabPais).setIndicator("PAIS", null));
-
-        final Spinner spnMun = (Spinner)findViewById(R.id.spnMun);
-
-        Spinner spnDepto = (Spinner)findViewById(R.id.spnDepto);
-        spnDepto.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(sensor==null){
+            finish();
+        }
+           final TextView lblSensorProximidad = (TextView)findViewById(R.id.lblSensorProximidad);
+           sensorEventListener = new SensorEventListener() {
                @Override
-               public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                    spnMun.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,miDireccion.obtenerMunicipio(position) ));
-                   Toast.makeText(getApplicationContext(), "Indice: "+ position, Toast.LENGTH_LONG).show();
+               public void onSensorChanged(SensorEvent sensorEvent) {
+                    if( sensorEvent.values[0]>=0 && sensorEvent.values[0]<=4 ){
+                        getWindow().getDecorView().setBackgroundColor(Color.RED);
+                        lblSensorProximidad.setText("LEJOS: "+ sensorEvent.values[0]);
+                    } else if(sensorEvent.values[0]>4 && sensorEvent.values[0]<=8 ){
+                        getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                        lblSensorProximidad.setText("INTERMEDIO: "+ sensorEvent.values[0]);
+                    } else{
+                        getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                        lblSensorProximidad.setText("CERCA: "+ sensorEvent.values[0]);
+                    }
                }
-
                @Override
-               public void onNothingSelected(AdapterView<?> adapterView) {
+               public void onAccuracyChanged(Sensor sensor, int i) {
 
                }
-        });
-
-           Button btnObtenerPais = (Button)findViewById(R.id.btnObtenerPais);
-           btnObtenerPais.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View view) {
-                   Spinner spnPaises = (Spinner)findViewById(R.id.spnPaises);
-                   spnPaises.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,miDireccion.obtenerPaises()));
-               }
-           });
+           };
+           iniciar();
+    }
+    void iniciar(){
+           sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
-/*class direcciones{
-    String [][] mun = {
-            {"Seleccione un depto"},
-            {"Usulutan","Santa Maria", "Santa Elena", "Jiquilisco"},
-            {"El Transito", "San Jorge", "San Rafael"},
-            {"SRL","La Union","Anamoros","El Carmen"},
-            {""}
-    };
-    String[] obtenerMun(int posicion){
-        return mun[posicion];
-    }
-}*/
