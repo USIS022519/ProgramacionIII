25d9fc8874a25ec7a71a538eb3cd8f6d564dbae5
Luis Enrique Hernandez <sarco_luis@hotmail.com>
2020-08-26 10:17:01 -0600

    Reproductor de Musica en Android Studio

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,80 +1,48 @@
 package com.example.prueba;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.net.wifi.aware.PublishConfig;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    public Button btnCalcular;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnCalcular = (Button)findViewById(R.id.btnCalcular);
-        btnCalcular.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                procesar(view);
-            }
-        });
-    }
-    public void procesar(View vista){
         try {
-            RadioGroup optOperaciones = (RadioGroup) findViewById(R.id.optOperaciones);
-            Spinner cboOperaciones = (Spinner)findViewById(R.id.cboOperaciones);
+            mediaPlayer = MediaPlayer.create(this, R.raw.patria_querida);
 
-            TextView tempVal = (TextView) findViewById(R.id.txtnum1);
-            double num1 = Double.parseDouble(tempVal.getText().toString());
-
-            tempVal = (TextView) findViewById(R.id.txtnum2);
-            double num2 = Double.parseDouble(tempVal.getText().toString());
-
-            double respuesta = 0;
-            //Este es para el radiogroup y los radiobuttons
-            switch (optOperaciones.getCheckedRadioButtonId()) {
-                case R.id.optSuma:
-                    respuesta = num1 + num2;
-                    break;
-                case R.id.optResta:
-                    respuesta = num1 - num2;
-                    break;
-                case R.id.optMultiplicar:
-                    respuesta = num1 * num2;
-                    break;
-                case R.id.optDivision:
-                    respuesta = num1 / num2;
-                    break;
-            }
-            //Este es para el spinner... -> Combobox.
-            switch (cboOperaciones.getSelectedItemPosition()){
-                case 1: //suma
-                    respuesta = num1 + num2;
-                    break;
-                case 2: //resta
-                    respuesta = num1 - num2;
-                    break;
-                case 3: //multiplicacion
-                    respuesta = num1 * num2;
-                    break;
-                case 4: //division
-                    respuesta = num1 / num2;
-                    break;
-            }
-            tempVal = (TextView) findViewById(R.id.lblRespuesta);
-            tempVal.setText("Respuesta: " + respuesta);
-        }catch (Exception err){
-            TextView temp = (TextView) findViewById(R.id.lblRespuesta);
-            temp.setText("Por favor ingrese los numeros correspondientes.");
-
-            Toast.makeText(getApplicationContext(),"Por favor ingrese los numeros.",Toast.LENGTH_LONG).show();
+            final Button btnReproducir = (Button) findViewById(R.id.btnReproducir);
+            btnReproducir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (!mediaPlayer.isPlaying()) {
+                        iniciar();
+                        btnReproducir.setBackgroundResource(R.drawable.pause);
+                    } else {
+                        parar();
+                        btnReproducir.setBackgroundResource(R.drawable.play);
+                    }
+                }
+            });
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Error: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
         }
+    }
+    void iniciar(){
+        mediaPlayer.start();
+    }
+    void parar(){
+        mediaPlayer.pause();
     }
 }
