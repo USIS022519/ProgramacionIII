3189ea0c538ab18b206805bbbd0a7fa3246e68d1
Luis Enrique Hernandez <sarco_luis@hotmail.com>
2020-09-11 14:00:10 -0600

    Creacion y publicacion de la rama AgendaAmigosSqLite

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -17,57 +17,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
-
-    @Override
-    protected void onPause() {
-        detener();
-        super.onPause();
-    }
-    @Override
-       protected void onResume() {
-        iniciar();
-        super.onResume();
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if(sensor==null){
-            finish();
-        }
-           final TextView lblSensorLuz = (TextView)findViewById(R.id.lblSensorLuz);
-           sensorEventListener = new SensorEventListener() {
-               @Override
-               public void onSensorChanged(SensorEvent sensorEvent) {
-                   double luz = sensorEvent.values[0];
-                    if(luz>=0 && luz<=15000){
-                        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
-                    }
-                   if(luz>=15000 && luz<=30000){
-                       getWindow().getDecorView().setBackgroundColor(Color.GRAY);
-                   }
-                   if(luz>=30000 && luz<=50000){
-                       getWindow().getDecorView().setBackgroundColor(Color.WHITE);
-                   }
-                   lblSensorLuz.setText("VALOR: " + luz);
-               }
-               @Override
-               public void onAccuracyChanged(Sensor sensor, int i) {
-
-               }
-           };
-           iniciar();
-    }
-    void iniciar(){
-           sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-    }
-    void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
     }
 }
