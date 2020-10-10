91da8b81dc405a5998d15a6a751823ddbf217668
Luis Enrique Hernandez <sarco_luis@hotmail.com>
2020-10-09 20:03:50 -0600

    Terminacion del CRUD con CouchDB

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.prueba;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.wifi.aware.PublishConfig;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -21,17 +27,22 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends Activity {
     JSONArray datosJSON;
     JSONObject jsonObject;
-
+    Integer posicion;
+    ArrayList<String> arrayList =new ArrayList<String>();
+    ArrayList<String> copyStringArrayList = new ArrayList<String>();
+    ArrayAdapter<String> stringArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +56,76 @@ public class MainActivity extends Activity {
         btnAgregarNuevoAmigos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                agregarNuevosAmigos();
+                agregarNuevosAmigos("nuevo", jsonObject);
+            }
+        });
+        buscarAmigos();
+    }
+    void buscarAmigos(){
+        final TextView tempVal = (TextView)findViewById(R.id.txtBuscarAmigo);
+        tempVal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                arrayList.clear();
+                if( tempVal.getText().toString().trim().length()<1 ){//no hay texto para buscar
+                    arrayList.addAll(copyStringArrayList);
+                } else{//hacemos la busqueda
+                    for (String amigo : copyStringArrayList){
+                        if(amigo.toLowerCase().contains(tempVal.getText().toString().trim().toLowerCase())){
+                            arrayList.add(amigo);
+                        }
+                    }
+                }
+                stringArrayAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_principal, menu);
+        try {
+            AdapterView.AdapterContextMenuInfo adapterContextMenuInfo = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            posicion = adapterContextMenuInfo.position;
+            menu.setHeaderTitle(datosJSON.getJSONObject(posicion).getString("nombre"));
+        }catch (Exception ex){
+
+        }
+    }
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnxAgregarAmigo:
+                agregarNuevosAmigos("nuevo", jsonObject);
+                return true;
+
+            case R.id.mnxModificarAmigo:
+                try {
+                    agregarNuevosAmigos("modificar", datosJSON.getJSONObject(posicion));
+                }catch (Exception ex){}
+                return true;
+
+            case R.id.mnxEliminarAmigo:
+
+                AlertDialog eliminarFriend =  eliminarAmigo();
+                eliminarFriend.show();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
     private class obtenerDatosAmigos extends AsyncTask<Void,Void, String> {
         HttpURLConnection urlConnection;
 
@@ -56,7 +133,7 @@ public class MainActivity extends Activity {
         protected String doInBackground(Void... voids) {
             StringBuilder result = new StringBuilder();
             try {
-                URL url = new URL("Http://192.168.1.15:5984/db_agenda/_design/agenda/_view/mi-agenda");
+                URL url = new URL("Http://192.168.1.7:5984/db_agenda/_design/agenda/_view/mi-agenda");
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
 
@@ -87,21 +164,112 @@ public class MainActivity extends Activity {
     private void mostrarDatosAmigos(){
         ListView ltsAmigos = findViewById(R.id.ltsAgendaAmigosCouchDB);
         try {
-            final ArrayList<String> arrayList = new ArrayList<>();
-            final ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+            arrayList.clear();
+            stringArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
             ltsAmigos.setAdapter(stringArrayAdapter);
 
             for (int i = 0; i < datosJSON.length(); i++) {
                 stringArrayAdapter.add(datosJSON.getJSONObject(i).getJSONObject("value").getString("nombre"));
             }
+            copyStringArrayList.clear();
+            copyStringArrayList.addAll(arrayList);
+
             stringArrayAdapter.notifyDataSetChanged();
             registerForContextMenu(ltsAmigos);
         }catch (Exception ex){
             Toast.makeText(MainActivity.this, "Error al mostrar los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
-    private void agregarNuevosAmigos(){
-        Intent nuevoAmigo = new Intent(MainActivity.this, agregar_amigos.class);
-        startActivity(nuevoAmigo);
+    public void agregarNuevosAmigos(String accion, JSONObject jsonObject){
+        try {
+            Bundle enviarParametros = new Bundle();
+            enviarParametros.putString("accion",accion);
+            enviarParametros.putString("dataAmigo",jsonObject.toString());
+
+            Intent agregarAmigo = new Intent(MainActivity.this, agregar_amigos.class);
+            agregarAmigo.putExtras(enviarParametros);
+            startActivity(agregarAmigo);
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Error al llamar agregar amigos: "+ e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+    AlertDialog eliminarAmigo(){
+        AlertDialog.Builder confirmacion = new AlertDialog.Builder(MainActivity.this);
+        try {
+            confirmacion.setTitle(datosJSON.getJSONObject(posicion).getJSONObject("value").getString("nombre"));
+            confirmacion.setMessage("Esta seguro de eliminar el registro?");
+            confirmacion.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    eliminarDatosAmigo objEliminarAmigo = new eliminarDatosAmigo();
+                    objEliminarAmigo.execute();
+
+                    Toast.makeText(getApplicationContext(), "Amigo eliminado con exito.", Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
+                }
+            });
+            confirmacion.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(getApplicationContext(), "Eliminacion cancelada por el usuario.", Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
+                }
+            });
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Error al mostrar la confoirmacion: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return confirmacion.create();
+    }
+    private class eliminarDatosAmigo extends AsyncTask<String,String, String> {
+        HttpURLConnection urlConnection;
+
+        @Override
+        protected String doInBackground(String... parametros) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String jsonResponse = null;
+            try {
+                URL url = new URL("http://192.168.1.7:5984/db_agenda/" +
+                        datosJSON.getJSONObject(posicion).getJSONObject("value").getString("_id") + "?rev=" +
+                        datosJSON.getJSONObject(posicion).getJSONObject("value").getString("_rev"));
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("DELETE");
+
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String inputLine;
+                StringBuffer stringBuffer = new StringBuffer();
+                while ((inputLine = reader.readLine()) != null) {
+                    stringBuffer.append(inputLine + "\n");
+                }
+                if (stringBuffer.length() == 0) {
+                    return null;
+                }
+                jsonResponse = stringBuffer.toString();
+                return jsonResponse;
+            } catch (Exception ex) {
+                //
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                if (jsonObject.getBoolean("ok")) {
+                    Toast.makeText(getApplicationContext(), "Datos de amigo guardado con exito", Toast.LENGTH_SHORT).show();
+                    obtenerDatosAmigos objObtenerAmigos = new obtenerDatosAmigos();
+                    objObtenerAmigos.execute();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error al intentar guardar datos de amigo", Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Error al guardar amigo: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/prueba/agregar_amigos.java
+++ app/src/main/java/com/example/prueba/agregar_amigos.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONObject;
@@ -17,32 +19,67 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class agregar_amigos extends AppCompatActivity {
-
+    String resp, accion, id, rev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_amigos);
 
-        FloatingActionButton btnMostrarAmigos = findViewById(R.id.btnMostrarAmigos);
-        btnMostrarAmigos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mostrarAmigos();
+       try {
+           FloatingActionButton btnMostrarAmigos = findViewById(R.id.btnMostrarAmigos);
+           btnMostrarAmigos.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   mostrarAmigos();
+               }
+           });
+           Button btnGuardarAmigo = findViewById(R.id.btnGuardarAmigos);
+           btnGuardarAmigo.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   guardarAmigo();
+               }
+           });
+           mostrarDatosAmigo();
+       }catch (Exception ex){
+           Toast.makeText(getApplicationContext(), "Error al agregar amigos: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
+       }
+    }
+    void mostrarDatosAmigo(){
+        try {
+            Bundle recibirParametros = getIntent().getExtras();
+            accion = recibirParametros.getString("accion");
+            if (accion.equals("modificar")){
+                JSONObject dataAmigo = new JSONObject(recibirParametros.getString("dataAmigo")).getJSONObject("value");
+
+                TextView tempVal = (TextView)findViewById(R.id.txtCodigoAmigo);
+                tempVal.setText(dataAmigo.getString("codigo"));
+
+                tempVal = (TextView)findViewById(R.id.txtNombreAmigo);
+                tempVal.setText(dataAmigo.getString("nombre"));
+
+                tempVal = (TextView)findViewById(R.id.txtDireccionAmigo);
+                tempVal.setText(dataAmigo.getString("direccion"));
+
+                tempVal = (TextView)findViewById(R.id.txtTelefonoAmigo);
+                tempVal.setText(dataAmigo.getString("telefono"));
+
+                tempVal = (TextView)findViewById(R.id.txtDuiAmigo);
+                tempVal.setText(dataAmigo.getString("dui"));
+
+                id = dataAmigo.getString("_id");
+                rev = dataAmigo.getString("_rev");
             }
-        });
-        FloatingActionButton btnGuardarAmigo = findViewById(R.id.btnGuardarAmigos);
-        btnGuardarAmigo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                guardarAmigo();
-            }
-        });
+        }catch (Exception ex){
+            ///
+        }
     }
     private void mostrarAmigos(){
         Intent mostrarAmigos = new Intent(agregar_amigos.this, MainActivity.class);
@@ -66,12 +103,18 @@ public class agregar_amigos extends AppCompatActivity {
 
         try {
             JSONObject datosAmigo = new JSONObject();
+            if (accion.equals("modificar")){
+                datosAmigo.put("_id",id);
+                datosAmigo.put("_rev",rev);
+            }
             datosAmigo.put("codigo", codigo);
             datosAmigo.put("nombre", nombre);
             datosAmigo.put("direccion", direccion);
             datosAmigo.put("telefono", telefono);
             datosAmigo.put("dui", dui);
 
+            enviarDatosAmigo objGuardarAmigo = new enviarDatosAmigo();
+            objGuardarAmigo.execute(datosAmigo.toString());
         }catch (Exception ex){
             Toast.makeText(getApplicationContext(), "Error: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
         }
@@ -85,7 +128,7 @@ public class agregar_amigos extends AppCompatActivity {
             String jsonDatos = parametros[0];
             BufferedReader reader;
             try {
-                URL url = new URL("http://192.168.1.15:5984/db_agenda/");
+                URL url = new URL("http://192.168.1.7:5984/db_agenda/");
                 urlConnection = (HttpURLConnection)url.openConnection();
                 urlConnection.setDoInput(true);
                 urlConnection.setDoOutput(true);
@@ -99,15 +142,41 @@ public class agregar_amigos extends AppCompatActivity {
                 writer.close();
 
                 InputStream inputStream = urlConnection.getInputStream();
+                if(inputStream==null){
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                resp = reader.toString();
+
+                String inputLine;
+                StringBuffer stringBuffer = new StringBuffer();
+                while ((inputLine=reader.readLine())!= null){
+                    stringBuffer.append(inputLine+"\n");
+                }
+                if(stringBuffer.length()==0){
+                    return null;
+                }
+                jsonResponse = stringBuffer.toString();
+                return jsonResponse;
             }catch (Exception ex){
                 //
             }
             return null;
         }
-
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            try{
+                JSONObject jsonObject = new JSONObject(s);
+                if(jsonObject.getBoolean("ok")){
+                    Toast.makeText(getApplicationContext(), "Datos de amigo guardado con exito", Toast.LENGTH_SHORT).show();
+                    mostrarAmigos();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error al intentar guardar datos de amigo", Toast.LENGTH_SHORT).show();
+                }
+            }catch (Exception e){
+                Toast.makeText(getApplicationContext(), "Error al guardar amigo: "+e.getMessage(), Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
